1ba50917f2c479c995fd7ea29c2c238590f001b6
Michael Phan <sonpython@gmail.com>
2021-06-16 12:25:54 +0700

    add mtail update absolute path for run_mtail.sh

--- data_collect/run_mtail.sh
+++ data_collect/run_mtail.sh
@@ -1,4 +1,4 @@
 # ~/.chia/mainnet/log/ is where your Chia logs saved.
 # Replace them if they are different on your host
-nohup watch -n 30 "../venv/bin/chia farm summary > farmer_summary.log" &
-nohup ./mtail --progs ./ --logs ~/.chia/mainnet/log/debug.log --logs ./farmer_summary.log --poll_interval 3000ms &
+nohup watch -n 30 "~/chia-blockchain/venv/bin/chia farm summary > ~/.chia/mainnet/log/farmer_summary.log" &
+nohup ./mtail --progs ./ --logs ~/.chia/mainnet/log/debug.log --logs ~/.chia/mainnet/log/farmer_summary.log --poll_interval 3000ms &
